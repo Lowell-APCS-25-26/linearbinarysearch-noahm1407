6ff9a791aaafda978023dad5b2156c57af621aa0
noahm1407 <noahmuchowski@gmail.com>
2026-03-20 16:00:54 +0000

    Code doesn't work with current test cases but I th ink it should

--- src/main/java/Sketch.java
+++ src/main/java/Sketch.java
@@ -24,20 +24,52 @@ public class Sketch {
     new Item(19967, 45)
   };                             
   public int linearSearch(int catNumToFind){
-    //complete this method
-    return -1;
+  for (int i = 0; i < store.length; i++)
+    if(store[i].getCatNum() == catNumToFind)
+      return store[i].getInventory();
+  
+  return -1;
   }
   public int recursiveLinearSearch(int catNumToFind, int startIndex){
-    //complete this method
+  if (startIndex >= store.length)
     return -1;
+  else if (store[startIndex].getCatNum() == catNumToFind)
+    return store[startIndex].getInventory();
+  
+    return recursiveLinearSearch(catNumToFind,startIndex+1);
   }
   public int binarySearch(int catNumToFind){
-    //complete this method    
-    return -1;
+  int low = 0;
+  int high = store.length - 1;
+  int guess = 0;
+  
+  while (low <= high){
+  
+  guess = (low + high)/2;
+  if (store[guess].getCatNum() == catNumToFind)
+  return store[guess].getInventory();
+  else if (store[guess].getCatNum() < catNumToFind)
+  low = guess + 1;
+  else if (store[guess].getCatNum() > catNumToFind)
+  high = guess - 1;
+  
+  }
+  
+  return -1;
   }
   public int recursiveBinarySearch(int catNumToFind, int nLow, int nHigh){
-    //complete this method    
-    return -1;
+  int guess = (nLow + (nHigh-nLow)/2);
+  
+  if (nLow > nHigh)
+  return -1;
+  else if (store[guess].getCatNum() == catNumToFind)
+  return store[guess].getInventory();
+  else if (store[guess].getCatNum() < catNumToFind)
+  return recursiveBinarySearch(catNumToFind,guess+1,nHigh);
+  else if (store[guess].getCatNum() > catNumToFind)
+  return recursiveBinarySearch(catNumToFind,nLow,guess-1);
+  else
+  return -1;
   }
   public void tester(){
     int[] tests = {0, 183, 184, 2370, 15320, 19967, 19968};
